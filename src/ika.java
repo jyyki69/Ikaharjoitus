3dabd98b550b5292a5ba795dee9e09d634ab8d8e
Jyrki Lampela <142908775+jyyki69@users.noreply.github.com>
2023-09-25 17:48:27 +0300

    Initial commit

---
+++ src/ika.java
@@ -0,0 +1,82 @@
+
+public class ika {
+
+	public static void main(String[] args) {
+		// Mitä kukakin saa tehdä iän perusteella
+		
+		int ika = 40;
+				
+			
+		
+		// Tulostusehdot
+		
+		//alaikäiset
+		if (ika >= 0 && ika < 18)
+		{
+				System.out.println("Olet alaikäinen.");
+			if (ika >=15)
+				System.out.println("Saat ajaa mopedia.");
+			if (ika >=16) 
+				System.out.println("Saat ajaa kevarilla.");
+		}	
+		
+		//täysi-ikäiset
+		
+//		else
+		if (ika >= 18)	
+		{
+			System.out.println("Olet aikuinen.");
+			
+			 if (ika ==18)
+				System.out.println("Pääset ajamaan autoa täysi-ikäisenä!");	
+			 if (ika ==58)
+				 System.out.println("Pääsisit jo varhaiseläkkeelle!");
+			 if (ika ==40)
+				 System.out.println("Nelkyt on uus kolkyt!");
+			 if (ika == 50)
+				 System.out.println("Hyvää keski-ikää vaan!");
+			 
+		}
+		
+		//eläkeläiset rocks
+			 
+		if (ika > 65)
+			{	
+			System.out.println("Olet eläkeläinen.");
+			}
+	
+		if (ika ==65)
+			{
+				System.out.println("Eläkkeellemenokakkukahveet!");
+			}
+			else if (ika == 100)
+			{	
+				System.out.println("SE ON");
+				System.out.println("TASAN");
+				System.out.println("SATA VUOTTA!");
+				System.out.println("EIKÄ");
+				System.out.println("AIVAN");
+				System.out.println("SUOTTA!");
+		}
+	}
+}
+
+
+	    
+		
+
+		   
+		    
+				
+		
+	
+	
+
+
+
+
+
+
+	
+
+
